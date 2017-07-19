a76b528c3afa9ede781d82b08cd0051d791ab76d
Connor Kuehl <cipkuehl@gmail.com>
2017-07-19 15:35:57 -0700

    Add Vertex, Edge, Graph Generic classes.

--- Assets/Scripts/LevelGeneration/Graph.cs
+++ Assets/Scripts/LevelGeneration/Graph.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System.Collections.Generic;
 
-public class Graph : MonoBehaviour {
+public class Graph<T> {
+    private List<Vertex<T>> adjacencyList;
 
-	// Use this for initialization
-	void Start() {
-		
-	}
-	
-	// Update is called once per frame
-	void Update() {
-		
-	}
+    public Graph() {
+        adjacencyList = new List<Vertex<T>>();
+    }
+
+    public Graph(int initialSize) {
+        if (initialSize > 0) {
+            adjacencyList = new List<Vertex<T>>(initialSize);
+        }
+    }
+
+    private class Vertex<T> {
+        public T Data { public get; private set; }
+
+        private List<Edge> edgeList;
+    }
+
+    private class Edge {
+         public int AdjacentTo { get; set; }
+    }
 }
