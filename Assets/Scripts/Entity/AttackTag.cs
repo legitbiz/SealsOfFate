b4cb8714f086291a46553950892fc643372c0a0c
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2017-07-22 07:46:57 -0700

    More spikes on #56
    - Adds attack effects and tags
    - Adds defense effects and tags
    - Adds basic combat implementation in CombatData.ComputeDamage

---
+++ Assets/Scripts/Entity/AttackTag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Entity
+{
+    internal abstract class AttackTag
+    {
+        public abstract void Apply(ref CombatData cd);
+    }
+    
+    internal class UnblockableAttack : AttackTag
+    {
+        public override void Apply(ref CombatData cd)
+        {
+            cd.Armor = 0;
+        }
+    }
+}
