58eebcfa8701404c290feb6019fa873103bd9cfb
Andrew Waugh <ZonrZero@gmail.com>
2017-07-26 21:57:10 -0700

    Reworks the BucketQueue to be backed by different data structures

--- Assets/Scripts/Utility/BucketQueue.cs
+++ Assets/Scripts/Utility/BucketQueue.cs
@@ -14,22 +14,20 @@ namespace Assets.Scripts.Utility
     /// </summary>
     /// <typeparam name="T">The data type of the BucketQueue</typeparam>
     class BucketQueue<T> : IEnumerable<T> {
+        public int Count { get; set; }
         /// <summary>The initial number of buckets. Backed by C# List implementation so expands as needed</summary>
-        private int _numBuckets;
         /// <summary>The initial size of each of the buckets. Expands as needed.</summary>
         private int _bucketSize;
         /// <summary>The actual array of queues</summary>
-        private readonly List<Queue<T>> Buckets;
+        private readonly SortedDictionary<int, List<T>> Buckets;
 
         /// <summary>
         /// CTor for the BucketQueue. Initializes the data structure and sets default sizes.
         /// </summary>
-        /// <param name="initialBuckets">The initial number of obuckets. Expands as needed. Default 25</param>
         /// <param name="initialBucketSize">The intial size of each of the buckets. Expands as needed. Default 10</param>
-        public BucketQueue(int initialBuckets = 25,int initialBucketSize = 10) {
-            _numBuckets = initialBuckets;
+        public BucketQueue(int initialBucketSize = 10) {
             _bucketSize = initialBucketSize;
-            Buckets = new List<Queue<T>>(_numBuckets);
+            Buckets = new SortedDictionary<int, List<T>>();
             
         }
 
@@ -39,10 +37,12 @@ namespace Assets.Scripts.Utility
         /// <param name="toAdd">The data item to add</param>
         /// <param name="key">The key to sort the new data item on</param>
         public void Enqueue(T toAdd,int key) {
-            if (Buckets[key] == null) {
-                Buckets[key] = new Queue<T>(_bucketSize);
+            Debug.Assert(key < 1000, "Key is not an insanely big value");
+            if (!Buckets.ContainsKey(key)) {
+                Buckets[key] = new List<T>(_bucketSize);
             }
-            Buckets[key].Enqueue(toAdd);
+            Buckets[key].Add(toAdd);
+            ++Count;
         }
 
         /// <summary>
@@ -50,11 +50,13 @@ namespace Assets.Scripts.Utility
         /// </summary>
         /// <returns>The data item removed from the queue</returns>
         public T Dequeue() {
-            int i = 0;
-            while (Buckets[i].Count == 0) {
-                ++i;
+            if (Count == 0) {
+                throw new InvalidOperationException("Empty Queue");
             }
-            return Buckets[i].Dequeue();
+            var ret = Peek();
+            Buckets.Values.First(b => b != null).RemoveAt(0);
+            --Count;
+            return ret;
         }
 
         /// <summary>
@@ -63,8 +65,10 @@ namespace Assets.Scripts.Utility
         /// <returns>An enumerator of the underlying object type</returns>
         public IEnumerator<T> GetEnumerator() {
             for (int i = 0; i < Buckets.Count; ++i) {
-                foreach (var itemInBucket in Buckets[i]) {
-                    yield return itemInBucket;
+                if (Buckets.ContainsKey(i)) {
+                    foreach (var itemInBucket in Buckets[i]) {
+                        yield return itemInBucket;
+                    }
                 }
             }
         }
@@ -75,6 +79,30 @@ namespace Assets.Scripts.Utility
         /// <returns>A basic Enumerator</returns>
         IEnumerator IEnumerable.GetEnumerator() {
             return GetEnumerator();
+        }
+
+        /// <summary>
+        /// Removes a single item from the queue.
+        /// </summary>
+        /// <param name="toRemove"></param>
+        /// <param name="key"></param>
+        public void Remove(T toRemove, int key) {
+            if (!Buckets.ContainsKey(key) || Buckets[key] == null) {
+                throw new ArgumentException("key");
+            }
+            --Count;
+            Buckets[key].Remove(toRemove);
+        }
+
+        /// <summary>
+        /// Returns the first item in the queue
+        /// </summary>
+        /// <returns>The item at the front of the queue</returns>
+        public T Peek() {
+            int i = 0;
+            if (Count == 0) {
+                throw new InvalidOperationException("Empty Queue");}
+            return Buckets.Values.First(b => b != null).First();
         }
     }
 }
